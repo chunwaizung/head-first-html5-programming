5251a77ad333c96e3951e51132c45082d0662c97
chunwaizung <zjw3298@me.com>
2016-01-17 13:38:52 +0800

    删除XMLHttpRequest,使用JSONP

--- chapter06/mightygumball.js
+++ chapter06/mightygumball.js
@@ -6,20 +6,12 @@
 
 window.onload = function() {
 	var url = "http://gumball.wickedlysmart.com／gumball/gumball.com";
-	//var url = "http://localhost/sales.json"; //测试时需要在浏览器输入http://localhost/mightygumball.html,直接打开mightygumball.html将无法获取json数据
- 	var request = new XMLHttpRequest();
- 	request.open("GET", url);
- 	request.onload = function() {
- 		if (request.status == 200) {
- 			updateSales(request.responseText);
- 		}
- 	};
- 	request.send(null);
+
 }
 
-function updateSales(responseText) {
+function updateSales(sales) { //返回的新数据不再是一个json串，而是一个对象。
  	var salesDiv = document.getElementById("sales");
- 	var sales = JSON.parse(responseText); //转化为javascript对象
+
  	for (var i = 0; i < sales.length; i++) { //迭代处理每一个数组元素
  		var sale = sales[i];
  		var div = document.createElement("div"); //对每个元素，创建一个div，设置class属性以便css调整样式
